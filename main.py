7bd828c5a27a4e78924e59e97230f7c630558b22
adinugrahadharmaputra <adinugrahadharmaputra@gmail.com>
2022-02-16 23:38:13 +0700

    setup

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix= "pa")
+client = commands.Bot(command_prefix= "pa ")
 
-xclien
+@client.event
+async def on_ready():
+    print("Im Ready |^_^|")
+    print("--------------")
+@client.command()
+async def hello(ctx):  
+    await ctx.send("Hello, I am Proto Alpha")
+
+client.run('OTQzNTAwNDk1NDY3NzA4NDM3.Ygz9Tw.yjtEeiDMO3gBHD2gV0hpHhC8A74')
